f4092ed0f91f88de528633ebf606fbe9f82bd49f
NetAcad DEVASC <changeme@example.com>
2026-01-22 20:38:19 +0000

    web app

---
+++ sample-app/sample-app.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp sample-app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python:3.11-slim" > tempdir/Dockerfile
+echo "RUN pip install --no-cache-dir --progress-bar off flask" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  sample-app.py /home/myapp/" >> tempdir/Dockerfile
+echo "WORKDIR /home/myapp" >> tempdir/Dockerfile
+echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/sample-app.py" >> tempdir/Dockerfile
+
+cd tempdir
+docker build -t sampleapp .
+
+docker run -t -d -p 8080:8080 --name samplerunning sampleapp
+docker ps -a
